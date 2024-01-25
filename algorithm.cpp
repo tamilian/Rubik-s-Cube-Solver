c946c3a4251799ab030c510dc194787698fc717c
tamilian <nttamilian@gmail.com>
2024-01-24 18:54:12 -0600

    Update algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-
 // globals
 const int DIMENSION1 = 6;
 const int DIMENSION2 = 3;
@@ -26,7 +25,7 @@ const int DIMENSION3 = 3;
 
 // starting state is depth 0
 
-void takeInput(int ***initialState) {
+void takeInput(int*** initialState) {
 
     ifstream inputFile("input.txt");
 
@@ -64,12 +63,12 @@ void takeInput(int ***initialState) {
 }
 
 // ability to print any cube that is given
-void print(int *** array){
+void print(int*** array){
 
 
     for (int i = 0; i < DIMENSION1; i++){
-        for (int j = 0; j < DIMENSION2){
-            for (int k = 0; k < DIMENSION3){
+        for (int j = 0; j < DIMENSION2; j++){
+            for (int k = 0; k < DIMENSION3; k++){
               cout << array[i][j][k];
             }
         }
@@ -83,17 +82,51 @@ void print(int *** array){
 
 
 // now we need to come up as many moves as possible and how the transpose 
-// all the moves are with respective to the green phase
+// all the moves are with respective to the green center piece
 // it was discussed that that was all we really needed, it ticks the boxes for all the moves that are needed
 
 
 // MOVES
 
+
+// index map of app pieces to help with writing transpositin functions 
+
+    std::string multilineString = R"(
+        
+rubiksCube[0][0][0] = W   rubiksCube[0][0][1] = W   rubiksCube[0][0][2] = W
+rubiksCube[0][1][0] = W   rubiksCube[0][1][1] = W   rubiksCube[0][1][2] = W
+rubiksCube[0][2][0] = W   rubiksCube[0][2][1] = W   rubiksCube[0][2][2] = W
+
+rubiksCube[1][0][0] = G   rubiksCube[1][0][1] = G   rubiksCube[1][0][2] = G
+rubiksCube[1][1][0] = G   rubiksCube[1][1][1] = G   rubiksCube[1][1][2] = G
+rubiksCube[1][2][0] = G   rubiksCube[1][2][1] = G   rubiksCube[1][2][2] = G
+
+rubiksCube[2][0][0] = R   rubiksCube[2][0][1] = R   rubiksCube[2][0][2] = R
+rubiksCube[2][1][0] = R   rubiksCube[2][1][1] = R   rubiksCube[2][1][2] = R
+rubiksCube[2][2][0] = R   rubiksCube[2][2][1] = R   rubiksCube[2][2][2] = R
+
+rubiksCube[3][0][0] = B   rubiksCube[3][0][1] = B   rubiksCube[3][0][2] = B
+rubiksCube[3][1][0] = B   rubiksCube[3][1][1] = B   rubiksCube[3][1][2] = B
+rubiksCube[3][2][0] = B   rubiksCube[3][2][1] = B   rubiksCube[3][2][2] = B
+
+rubiksCube[4][0][0] = O   rubiksCube[4][0][1] = O   rubiksCube[4][0][2] = O
+rubiksCube[4][1][0] = O   rubiksCube[4][1][1] = O   rubiksCube[4][1][2] = O
+rubiksCube[4][2][0] = O   rubiksCube[4][2][1] = O   rubiksCube[4][2][2] = O
+
+rubiksCube[5][0][0] = Y   rubiksCube[5][0][1] = Y   rubiksCube[5][0][2] = Y
+rubiksCube[5][1][0] = Y   rubiksCube[5][1][1] = Y   rubiksCube[5][1][2] = Y
+rubiksCube[5][2][0] = Y   rubiksCube[5][2][1] = Y   rubiksCube[5][2][2] = Y
+     
+    )";
+
+
+
 int*** left(int*** cube){
 
-
 // allocate memory for new 3D array
-int *** temp;
+
+// when a new move is made, there are 20 coordinates in array that are moved (differeeb
+int*** temp;
 
 temp = new int **[DIMENSION1];
 
@@ -110,41 +143,236 @@ temp = new int **[DIMENSION1];
     for (int i = 0; i < DIMENSION1; i++){
         for (int j = 0; j < DIMENSION2; j++){
             for (int k = 0; k < DIMENSION3; k++){
-            temp[i][j][k] = cube[i][j][k]
+            temp[i][j][k] = cube[i][j][k];
             }
         }
     }
 
-    // now we transpose 
-    
+    // now we transpose! 
 
+    // new          original 
+    
+    temp[2][0][2] = cube[5][0][2];
+    temp[2][1][2] = cube[5][1][2];
+    temp[2][2][2] = cube[5][2][2];
+
+    temp[0][0][2] = cube[2][j][k];
+    temp[0][1][2] = cube[2][j][k];
+    temp[0][2][2] = cube[2][j][k];
+
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
+    temp[i][j][k] = cube[i][j][k];
 
 
 }
 
 
+int*** left_prime(){
+    
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
 
 
 
-int*** left_prime();
+int*** right(){
+    
 
-int*** right();
+int*** temp;
 
-int*** right_prime();
+temp = new int **[DIMENSION1];
 
-int*** up();
+    for (int i = 0; i < DIMENSION1; i++){
 
-int*** up_prime();
+        temp[i] = new int *[DIMENSION2];
 
-int*** down();
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
 
-int*** down_prime();
+int*** right_prime(){
 
-int*** front();
+int*** temp;
 
-int*** front_prime();
+temp = new int **[DIMENSION1];
 
-int*** back();
+    for (int i = 0; i < DIMENSION1; i++){
 
-int*** back_prime();
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** up(){
+
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** up_prime(){
+    
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** down(){
+
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** down_prime(){
+    
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** front(){
+    
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** front_prime(){
+    
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** back(){
+    
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+int*** back_prime(){
+    
+int*** temp;
+
+temp = new int **[DIMENSION1];
+
+    for (int i = 0; i < DIMENSION1; i++){
+
+        temp[i] = new int *[DIMENSION2];
+
+        for (int j = 0; j < DIMENSION2; j++){
+            temp[i][j] = new int[DIMENSION3];
+        
+        }
+    }
+}
+
+
+
+
 
